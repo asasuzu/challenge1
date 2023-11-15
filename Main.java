372e2b4332ed693ede73f799e59bb934faee409e
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-11-15 20:04:38 +0000

    practiceJava8

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
-  public static void main(String[] args){
-    int[] numbers = {3, 4, 9};
-    System.out.println("一桁の数字を入力してください");
-    int input = new java.util.Scanner(System.in).nextInt();
-    for (int value : numbers) {
-      if (value == input){
-        System.out.println("あたり");
-      }
-    }
+  public static void main (String[] args){
+    double triangleArea = calcTriangleArea(20.0, 5.0);
+    System.out.println("三角形の面積：" + triangleArea + "平方cm");
+    double circleArea = calcCircleArea(5.0);
+    System.out.println("えんの面積：" + circleArea + "平方cm");
+  }
+  public static double calcTriangleArea (double bottom, double height) {
+    double area = (bottom * height) /2;
+    return area;
+  }
+  public static double calcCircleArea (double radius) {
+    double area = radius * radius * 3.14;
+    return area;
   }
 }
