46044d8903cbb1ce5abe17463e38ae39cdfcc5de
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-12-04 10:03:31 +0000

    practiceJava15

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-public class Main {
-  public static void main(String[] args){
-    int[] numbers = {3,4,9};
-    System.out.println("1桁の数字を入力してください");
-    int input = new java.util.Scanner(System.in).nextInt();
-    for (int n:numbers){
-      if(n == input) {
-        System.out.println("あたり");
+public class Main{
+  public static void main(String[] args) {
+    int i = 1;
+    while (true) {
+      if(i < 1000) {
+        break;
       }
-    }
+      i = i * 2;
+    }  
+    System.out.println(i);
   }
 }
